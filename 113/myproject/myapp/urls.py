b64b2cc287adb93577147e8f1ced24e3da7e1b86
Leo <75311991+wendy062644@users.noreply.github.com>
2025-04-01 19:44:18 +0800

    Delete 113 directory

--- 113/myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('predict/', views.predict_future_data, name='predict'),
-    path('cmp_code/', views.compare_code, name='compare_code'),
-]
